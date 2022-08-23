06a345455aa52d539cbbff84b2c17d363a047d90
Ihor Voznyi <devphase.sec@gmail.com>
2022-08-23 19:57:53 +0300

    Renamed Folder

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,5 +3,5 @@ export { default as Header } from './Header/Header';
 export { default as Menu } from './Menu/Menu';
 export { default as CardContainer } from './CardContainer/CardContainer';
 export { default as Loader } from './Loader/Loader';
-export { default as Apartment } from './Apartment/Default/Apartment';
-export { default as EditForm } from './Apartment/EditForm/EditForm';
+export { default as Apartment } from './ApartmentDetail/Default/Apartment';
+export { default as EditForm } from './ApartmentDetail/EditForm/EditForm';
